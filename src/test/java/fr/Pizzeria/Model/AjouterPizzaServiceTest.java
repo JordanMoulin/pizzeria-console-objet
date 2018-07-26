b71040f5c65086a1ff950f5612772c74ecf3391d
Moulin Jordan <jordan.moulin72@gmail.com>
2018-07-26 16:52:59 +0200

    Validation JUnit AjouterPizzaService

--- src/test/java/fr/Pizzeria/Model/AjouterPizzaServiceTest.java
+++ src/test/java/fr/Pizzeria/Model/AjouterPizzaServiceTest.java
@@ -5,19 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.Pizzeria.Exception.SavePizzaException;
+
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
 
 import java.util.Scanner;
 
 public class AjouterPizzaServiceTest {
-
 	@Rule
 	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+
 	@Test
 	public void testExecuteUC() {
+		PizzaMemDao dao = new PizzaMemDao();
+		assertEquals(8,dao.findAllPizzas().size());
 		systemInMock.provideLines("TES","TEST","10","viande");
-		AjouterPizzaService serv = new AjouterPizzaService();
-		int value = serv.executeUC(new Scanner(System.in), pizzas);
+		AjouterPizzaService ajout = new AjouterPizzaService();
+		try {
+			ajout.executeUC(new Scanner(System.in), dao);
+			assertEquals(9,dao.findAllPizzas().size());
+		} catch (SavePizzaException e) {
+			System.out.println("echec");
+		}
+		
 	}
 
 }
